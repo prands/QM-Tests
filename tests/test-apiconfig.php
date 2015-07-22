ff953ecc045e4627eeaa0e4b1dde9f34ec70b811
satish <satish.rajput@dotsquares.com>
2015-07-22 13:06:53 +0530

    change in auth

--- tests/test-apiconfig.php
+++ tests/test-apiconfig.php
@@ -16,8 +16,7 @@ class ApiconfigTest extends WP_UnitTestCase {
     {
       
 	$url_params = http_build_query($this->clientConfig);
-	switch (strtolower(HTTP_UTIL)) {
-		case 'curl':
+
 			$url = URL_TOKEN . $url_params;
 			$curl = curl_init();
 			curl_setopt($curl, CURLOPT_URL, $url);
@@ -30,22 +29,6 @@ class ApiconfigTest extends WP_UnitTestCase {
 			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, (get_option('qmoa_http_util_verify_ssl') == 1 ? 2 : 0));
 			$result = curl_exec($curl);
 			break;
-		case 'stream-context':
-			$url = rtrim(URL_TOKEN, "?");
-			$opts = array('http' =>
-				array(
-					'method'  => 'POST',
-					'header'  => 'Content-type: application/x-www-form-urlencoded',
-					'content' => $url_params,
-				)
-			);
-			$context = $context  = stream_context_create($opts);
-			$result = @file_get_contents($url, false, $context);
-			if ($result === false) {
-				$qmoa->qmoa_end_login("Sorry, we couldn't log you in. Could not retrieve access token via stream context. Please notify the admin or try again later.");
-			}
-			break;
-	}
 	// parse the result:
 	$result_obj = json_decode($result, true); // PROVIDER SPECIFIC: QuantiModo encodes the access token result as json by default		
 	$access_token = $result_obj['access_token']; // PROVIDER SPECIFIC: this is how QuantiModo returns the access token KEEP THIS PROTECTED!			
