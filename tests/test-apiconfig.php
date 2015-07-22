845ecb0db8427663e4a4297eb1388b9cbe28327b
satish <satish.rajput@dotsquares.com>
2015-07-22 13:05:05 +0530

    test config 3

--- tests/test-apiconfig.php
+++ tests/test-apiconfig.php
@@ -1,14 +1,53 @@
 <?php
 class ApiconfigTest extends WP_UnitTestCase {
-     
-	 public function testConfig() {
-        $data = array('client_id' => getenv('CLIENT_ID'), 'client_secret' => getenv('CLIENT_SECRET'), 'authorize_endpoint' => 'https://staging.quantimo.do:443/api/oauth2/authorize?', 'token_endpoint' => 'https://staging.quantimo.do:443/api/oauth2/token?');
-        
-        $this->assertEquals(CLIENT_ID, $data->client_id);
-        $this->assertEquals(CLIENT_SECRET, $data->client_secret);
-        $this->assertEquals(URL_AUTH, $data->authorize_endpoint);
-        $this->assertEquals(URL_TOKEN, $data->token_endpoint);
-        $this->assertNull(REDIRECT_URI);
-        $this->assertFalse(HTTP_UTIL);
+     private $clientConfig;
+	 public function setUp()
+		{
+        $this->clientConfig = array(
+                'client_id' => getenv('CLIENT_ID'),
+                'client_secret' => getenv('CLIENT_SECRET'),
+                'authorize_endpoint' => "https://staging.quantimo.do:443/api/oauth2/authorize?",
+                'token_endpoint' => 'https://staging.quantimo.do:443/api/oauth2/token?',
+            );
+           
     }
+	 
+	 public function testGetAccessToken()
+    {
+      
+	$url_params = http_build_query($this->clientConfig);
+	switch (strtolower(HTTP_UTIL)) {
+		case 'curl':
+			$url = URL_TOKEN . $url_params;
+			$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+			// PROVIDER NORMALIZATION: PayPal requires Accept and Accept-Language headers, Reddit requires sending a User-Agent header
+			// PROVIDER NORMALIZATION: PayPal/Reddit requires sending the client id/secret via http basic authentication
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, (get_option('qmoa_http_util_verify_ssl') == 1 ? 1 : 0));
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, (get_option('qmoa_http_util_verify_ssl') == 1 ? 2 : 0));
+			$result = curl_exec($curl);
+			break;
+		case 'stream-context':
+			$url = rtrim(URL_TOKEN, "?");
+			$opts = array('http' =>
+				array(
+					'method'  => 'POST',
+					'header'  => 'Content-type: application/x-www-form-urlencoded',
+					'content' => $url_params,
+				)
+			);
+			$context = $context  = stream_context_create($opts);
+			$result = @file_get_contents($url, false, $context);
+			if ($result === false) {
+				$qmoa->qmoa_end_login("Sorry, we couldn't log you in. Could not retrieve access token via stream context. Please notify the admin or try again later.");
+			}
+			break;
+	}
+	// parse the result:
+	$result_obj = json_decode($result, true); // PROVIDER SPECIFIC: QuantiModo encodes the access token result as json by default		
+	$access_token = $result_obj['access_token']; // PROVIDER SPECIFIC: this is how QuantiModo returns the access token KEEP THIS PROTECTED!			
+    }	
 }
