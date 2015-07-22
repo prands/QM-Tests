2a74e7a9b6638f0ff4afaaf6139d4b5a82ce341b
satish <satish.rajput@dotsquares.com>
2015-07-22 16:19:42 +0530

    change in API test 4

--- tests/test-apiconfig.php
+++ tests/test-apiconfig.php
@@ -3,30 +3,35 @@ class ApiconfigTest extends WP_UnitTestCase {
      private $clientConfig;
 	 public function setUp()
 		{       
-		/*$this->clientConfig = array(
-		'grant_type' => 'authorization_code',
-		'client_id' => getenv('CLIENT_ID'),
-		'client_secret' => getenv('CLIENT_SECRET'),
-		'code' => uniqid('', true),
-		'redirect_uri' => "http://quantimodo.projectstatus.in/",
-	);*/
-		echo $this->testGetOauthCode($this);  die;
-    }
-	
-	function testGetOauthCode($qmoa) {
-	$params = array(
+		$this->clientConfig =  array(
 			'response_type' => 'code',
 			'client_id' => getenv('CLIENT_ID'),
 			'scope' => "writemeasurements",
 			'state' => uniqid('', true),
 			'redirect_uri' => "http://quantimodo.projectstatus.in/",
 		);
+		//echo $this->testGetOauthCode($this);  die;
+    }
+	
+	function testGetOauthCode($qmoa) {	
 		//$_SESSION['WPOA']['STATE'] = $params['state'];
 		
-		$url = "https://staging.quantimo.do:443/api/oauth2/authorize?" . http_build_query($params);
+		$url = "https://staging.quantimo.do:443/api/oauth2/authorize?" . http_build_query($this->clientConfig);
 		
-		header("Location: $url");
-		exit;
+		$curl = curl_init();
+			curl_setopt($curl, CURLOPT_URL, $url);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_POST, 1);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $this->clientConfig);
+			// PROVIDER NORMALIZATION: PayPal requires Accept and Accept-Language headers, Reddit requires sending a User-Agent header
+			// PROVIDER NORMALIZATION: PayPal/Reddit requires sending the client id/secret via http basic authentication
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, (get_option('qmoa_http_util_verify_ssl') == 1 ? 1 : 0));
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, (get_option('qmoa_http_util_verify_ssl') == 1 ? 2 : 0));
+			$result = curl_exec($curl);
+			
+	// parse the result:
+	$result_obj = json_decode($result, true); // PROVIDER SPECIFIC: QuantiModo encodes the access token result as json by default	
+	echo '<pre>'; print_r($result_obj); die;
 	}
 	
 	/*public function testGetAccessToken()
