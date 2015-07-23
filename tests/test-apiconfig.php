8df2217aaaefc448926c2bda49053aef21df8cb5
satish <satish.rajput@dotsquares.com>
2015-07-23 14:57:35 +0530

    check test

--- tests/test-apiconfig.php
+++ tests/test-apiconfig.php
@@ -28,12 +28,12 @@ class ApiconfigTest extends WP_UnitTestCase {
 			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, (get_option('qmoa_http_util_verify_ssl') == 1 ? 1 : 0));
 			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, (get_option('qmoa_http_util_verify_ssl') == 1 ? 2 : 0));
 			$result = curl_exec($curl);
-			
+			echo '<pre>'; print_r($result); die;
 			
 	// parse the result:
 	$result_obj = json_decode($result, true); // PROVIDER SPECIFIC: QuantiModo encodes the access token result as json by default	
-	$this->assertEquals(200, $result->getStatusCode());
-	//echo '<pre>'; print_r($_GET); die;
+	//$this->assertEquals(200, $result->getStatusCode());
+	
 	}
 	
 	/*public function testGetAccessToken()
