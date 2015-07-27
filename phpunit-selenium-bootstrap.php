87188581f9031ef39108e5b87d4f9446d00aad9e
satish <satish.rajput@dotsquares.com>
2015-07-27 16:00:27 +0530

    changes for selenium

--- phpunit-selenium-bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-PHPUnit_Extensions_SeleniumTestCase::shareSession(true);
-require_once 'tests/SeleniumTestCase/BaseTestCase.php';
-require_once 'tests/Selenium2TestCase/BaseTestCase.php';
-PHPUnit_Extensions_Selenium2TestCase::shareSession(true);
--- tests/Selenium2TestCase/Coverage/singleFileCoverage.php
+++
@@ -1,3 +0,0 @@
-<?php
-$_GET['PHPUNIT_SELENIUM_TEST_ID'] = 1;
-require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumCommon/phpunit_coverage.php';
