218715b82a849059863b658d9a76a04c943f0181
satish <satish.rajput@dotsquares.com>
2015-07-28 16:20:33 +0530

    ch bak

--- tests/selenium-test.php
+++ tests/selenium-test.php
@@ -2,7 +2,6 @@
 require __DIR__ . '/../../PHPUnit/Extensions/Selenium2TestCase.php';
 class WebTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-	die('HERE');
     protected function setUp()
     {
         $this->setBrowser('firefox');
